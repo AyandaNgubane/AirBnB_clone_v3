39795590bab2e9b178a564646ba5e327799469e2
Ayanda Ngubane <ayanda4me@gmail.com>
2024-02-24 04:17:00 -0800

    Task 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,6 +2,13 @@
 """First file"""
 from api.v1.views import app_views
 from flask import jsonify
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from models import storage
 
 
 @app_views.route('/status')
@@ -13,12 +20,11 @@ def status():
 @app_views.route('/stats')
 def total_objects():
     """ Retrieves the number of each objects by type """
-    stats = {
-        "Amenity": "amenities",
-        "City": "cities",
-        "Place": "places",
-        "Review": "reviews",
-        "State": "states",
-        "User": "users"
-    }
-    return jsonify({name: storage.count(cls) for cls, name in stats.items()})
+    classes = [Amenity, City, Place, Review, State, User]
+    cls_names = ["amenities", "cities", "places", "reviews", "states", "users"]
+
+    tot_objs = {}
+    for i in range(len(classes)):
+        tot_objs[cls_names[i]] = storage.count(classes[i])
+
+    return jsonify(tot_objs)
